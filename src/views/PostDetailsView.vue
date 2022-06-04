f4a961c6366420f76651bed090cff4c8d71b633c
canberk-yilmaz <cyilmaz089@gmail.com>
2022-06-05 01:08:45 +0300

    router exercise - about page change posts

---
+++ src/views/PostDetailsView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="post-detail">
+    <h1>Post Details</h1>
+    <textarea v-autofocus></textarea>
+    <p>id: {{ $route.params.id }}</p>
+    <p><RouterLink to="/posts">Go Back</RouterLink></p>
+  </div>
+</template>
+
+<script setup>
+// imports
+import { vAutofocus } from "../directives/vAutofocus";
+</script>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+    <textarea v-autofocus></textarea>
+
+    <ul>
+      <li>
+        <RouterLink to="/postDetails/id1">Post 1</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetails/id2">Post 2</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetails/id3">Post 3</RouterLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+// imports
+import { vAutofocus } from "../directives/vAutofocus";
+</script>
