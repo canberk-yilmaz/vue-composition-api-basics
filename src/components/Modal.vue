e105bdb8213fd34b8c2a6cfcde1483441eed0791
canberk-yilmaz <cyilmaz089@gmail.com>
2022-06-08 09:32:34 +0300

    composition api props

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,6 +2,7 @@
   <teleport to=".modals-container">
     <div class="modal">
       <h1><slot name="title" /></h1>
+      <h1>{{ props.title }}</h1>
       <slot />
       <!-- <pre>{{ $slots.title() }}</pre> -->
       <button>Hide modal</button>
@@ -9,13 +10,33 @@
   </teleport>
 </template>
 
+<!--
+Options api way of using props
+<script> 
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "No title specified",
+    },
+  },
+};
+</script>
+ -->
 <script setup>
-
 // To use the slot data in the script section we need to use the useSlots hook like below.
 // import { useSlots } from "vue";
 // const slots = useSlots();
 // console.log(slots.title());
 
+//props
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: "No title specified",
+  },
+});
 </script>
 
 <style scoped>
