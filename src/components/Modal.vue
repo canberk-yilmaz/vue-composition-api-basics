4c6b9597201e21ab611fe852f0c0bb1a2952cb00
canberk-yilmaz <cyilmaz089@gmail.com>
2022-06-08 09:06:33 +0300

    new component to spare modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+  <teleport to=".modals-container">
+    <div class="modal">
+      <h1>This is a modal</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis
+        molestias fugiat eaque quis possimus esse deleniti praesentium. Vel ad
+        illo tenetur corporis a culpa inventore quasi quisquam, debitis modi
+        eum?
+      </p>
+      <button>Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<style scoped>
+.modal {
+  background: beige;
+  padding: 1rem;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,38 +2,25 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show modals</button>
-    <teleport to=".modals-container">
-      <div v-if="showModal" class="modal">
-        <h1>This is a modal</h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis
-          molestias fugiat eaque quis possimus esse deleniti praesentium. Vel ad
-          illo tenetur corporis a culpa inventore quasi quisquam, debitis modi
-          eum?
-        </p>
-        <button @click="showModal = false">Hide modal</button>
-      </div>
-    </teleport>
+    <Modals v-if="showModal"></Modals>
   </div>
 </template>
 
+<!--
+<script>
+//options api
+import Modals from "@/components/Modal.vue";
+export default {
+  components: {
+    Modals,
+  },
+};
+</script>
+ -->
 <script setup>
 //imports
-
+import Modals from "@/components/Modal.vue";
 import { ref } from "vue";
 
 const showModal = ref(false);
 </script>
-
-<style scoped>
-.modal {
-  background: beige;
-  padding: 1rem;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-}
-</style>
